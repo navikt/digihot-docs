f89bce5b3a38cca26e25dd0da6b8d8d8c4b21581
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2021-06-17 17:52:26 +0200

    generer typescript

--- generer-datamodeller.sh
+++ generer-datamodeller.sh
@@ -6,11 +6,18 @@ rm -r hm-datamodell-kotlin/docs
 curl https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/5.1.0/openapi-generator-cli-5.1.0.jar -o openapi-generator-cli.jar
 
 java -jar openapi-generator-cli.jar generate \
-    -i openapi.yaml \
+    -i openapi.json \
     -g kotlin \
     -o hm-datamodell-kotlin \
     --additional-properties=enumPropertyNaming=UPPERCASE \
     --additional-properties=artifactId=hm-datamodell-kotlin \
     --additional-properties=groupId=com.github.navikt \
     --additional-properties=packageName=no.nav.hjelpemidler.domain \
+    --global-property=models
+
+java -jar openapi-generator-cli.jar generate \
+    -i openapi.json \
+    -g typescript-fetch \
+    -o hm-datamodell-typescript \
+    --additional-properties=enumPropertyNaming=UPPERCASE \
     --global-property=models
